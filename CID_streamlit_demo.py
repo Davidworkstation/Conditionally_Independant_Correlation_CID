8c383a25289c337564ceda7dc5ffbbce37e6b4f6
Risaiah <db2019@hawaii.edu>
2024-06-09 04:59:20 -0400

    up

--- CID_streamlit_demo.py
+++ CID_streamlit_demo.py
@@ -95,4 +95,4 @@ col2.plotly_chart(fig)
 correlation_matrix = df.corr()
 correlation_coefficient = correlation_matrix.loc['x1', 'x2']
 st.write("Corr(X1,X2):", correlation_coefficient)
-col2.write('The Conditionally Independent Copula (CID) is a statistical model used to describe the joint distribution of multiple variables. It assumes that the variables are marginally independent but can exhibit dependence when conditioned on a subset of variables. This allows CID to capture complex dependencies that traditional copulas may overlook.')
+col2.write('The Conditionally Independent Model (CID) is a statistical model used to describe the joint distribution of multiple variables. It assumes that the variables are marginally independent but can exhibit dependence when conditioned on a subset of variables. This allows CID to capture complex dependencies that traditional copulas may overlook.')
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Conditionally_Independant_Correlation_CID
- The Conditionally Independent Copula (CID) is a statistical model used to describe the joint distribution of multiple variables. It assumes that the variables are marginally independent but can exhibit dependence when conditioned on a subset of variables. This allows CID to capture complex dependencies that traditional copulas may overlook.
+ The Conditionally Independent model (CID) is a statistical model used to describe the joint distribution of multiple variables. It assumes that the variables are marginally independent but can exhibit dependence when conditioned on a subset of variables. This allows CID to capture complex dependencies that traditional models may overlook.
 
 ## Installation
 To install the project dependencies, run:
